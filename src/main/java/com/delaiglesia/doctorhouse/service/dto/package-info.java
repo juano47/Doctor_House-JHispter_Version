06125b193b109f67e6fdc468a381d9aabf6ba0b8
Juan Ignacio de la Iglesia <juan.delaiglesia.jdli@gmail.com>
2023-05-15 23:10:02 +0200

    Initial version of Doctor_House_JHipster_Version generated by generator-jhipster@7.9.3

---
+++ src/main/java/com/delaiglesia/doctorhouse/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.delaiglesia.doctorhouse.service.dto;
---
+++ src/main/java/com/delaiglesia/doctorhouse/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.delaiglesia.doctorhouse.web.rest.vm;
